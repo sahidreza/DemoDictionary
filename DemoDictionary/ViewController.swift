925233beba13663db835624c2f8e242c00a860f9
Sahid Reza <sahidreza@Sahids-MacBook-Air.local>
2021-06-15 11:27:08 +0530

    First task completed

---
+++ DemoDictionary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DemoDictionary
+//
+//  Created by Sahid Reza on 15/06/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
